2e033d03d553d3c8217730b076818927ff678ac6
cesarluc_ <juliocesaoguimaraes@gmail.com>
2021-01-08 16:58:17 -0300

    Adicionando OrderLocation

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react"
 import { fetchProducts } from "../api"
+import OrderLocation from "./OrderLocation"
 import ProductsList from "./ProductsList"
 import StepsHeader from "./StepsHeader"
 import "./styles.css"
-import {Product} from "./types"
+import {OrderLocationdata, Product} from "./types"
 
 function Orders (){
 
     const [products,setProducts] = useState<Product[]>([]);
     console.log(products)
+
+    const [orderLocation,setOrderLocation] = useState<OrderLocationdata>()
 
     useEffect(()=>{
         console.log("Componenente orders iniciou")
@@ -26,6 +29,7 @@ function Orders (){
         <div className="orders-container">
             <StepsHeader/>
             <ProductsList products={products}/>
+            <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
         </div>
      </>
     )
