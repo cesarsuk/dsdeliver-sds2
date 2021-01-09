8fe62cffab847475204c84a3208b11d55c93543f
cesarluc_ <juliocesaoguimaraes@gmail.com>
2021-01-09 14:53:57 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,15 @@
+import {ToastContainer} from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css';
+
 import './App.css';
-import Home from './Home';
-import Navbar from './Navbar';
 import Routes from './Routes';
+
 
 function App() {
   return (
     <>
       <Routes/>
+      <ToastContainer/>
     </>
   );
 }
