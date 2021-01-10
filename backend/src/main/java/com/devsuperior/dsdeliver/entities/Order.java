f83c6aa00a9c5ca76a2a4317eea881481bd42b06
cesarluc_ <juliocesaoguimaraes@gmail.com>
2021-01-10 11:40:24 -0300

    Correção total

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -91,7 +91,7 @@ public class Order implements Serializable{
 	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
-
+	
 	public OrderStatus getStatus() {
 		return status;
 	}
@@ -99,6 +99,16 @@ public class Order implements Serializable{
 	public void setStatus(OrderStatus status) {
 		this.status = status;
 	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for(Product p : products) {
+			sum = sum + p.getPrice();
+		}
+		return sum;
+	}
+	
+
 
 	public Set<Product> getProducts() {
 		return products;
